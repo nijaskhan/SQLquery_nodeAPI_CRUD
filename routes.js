d8ceec5b20e67b9cefca356b3a914bfdaf604544
nijaskhan <nijaskhan93@gmail.com>
2023-09-15 14:12:27 +0530

    add-user, get-userById api implemented

--- routes.js
+++ routes.js
@@ -1,23 +1,94 @@
 const router = require('express').Router();
 const client = require('./dbConfig');
 
+/* 
+    Pros of writing raw SQLqueries,
+        * Full Control over the queries we execute.
+        * writing raw SQL can lead to more efficient queries because you can tailor them to your database schema and indexing.
+        * For complex queries writing raw SQL can be more concise and easier to understand.
+        * Sometimes, you may need to use database-specific features or extensions that are not supported by ORMs. Writing raw SQL allows you to utilize these features directly.
+    Cons of writing raw SQLqueries,
+        * Writing raw SQL queries without proper input validation can lead to SQL injection vulnerabilities.
+        * Boilerplate Code
+
+    // SEQUELIZE IS A ONE OF THE ORMs that WE CAN USE.
+*/
+
 router.get('/users', (req, res) => {
-    try{
-        client.query('SELECT * FROM users', (err, response) => {
-            if(!err){
-                // console.log(response.rows);
+    try {
+        client.query('SELECT * FROM users', (err, data) => {
+            if (!err) {
+                // console.log(data.rows);
                 res.status(200).json({
                     success: true,
-                    data: response.rows
+                    data: data.rows
                 });
-            }else{
+            } else {
                 res.status(404).json({
                     success: false,
                     message: err.message
                 });
             }
         });
-    }catch(error){
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+});
+
+router.get('/user/:id', (req, res) => {
+    try {
+        client.query(`SELECT * FROM users WHERE id = ${req.params.id}`, (err, data) => {
+            if (!err) {
+                if (data.rows.length > 0) {
+                    res.status(200).json({
+                        success: true,
+                        data: data.rows
+                    });
+                } else {
+                    res.json({
+                        success: false,
+                        message: 'No such user'
+                    });
+                }
+            } else {
+                res.status(404).json({
+                    success: false,
+                    message: err.message
+                });
+            }
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+});
+
+router.post('/add-user', (req, res) => {
+    try {
+        const user = req.body;
+        const insertQuery = `INSERT INTO users (name, age, salary, department)
+        VALUES ('${user.name}', ${user.age}, ${user.salary}, 
+        (SELECT id FROM departments WHERE name='${user.department}'))`;
+
+        client.query(insertQuery, (err, data) => {
+            if (!err) {
+                res.status(200).json({
+                    success: true,
+                    message: `user added successfully`
+                });
+            } else {
+                res.status(500).json({
+                    success: false,
+                    message: err.message
+                });
+            }
+        });
+    } catch (error) {
         res.status(500).json({
             success: false,
             message: error.message
